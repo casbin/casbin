9b690a7c6619678364ea5b926cb7faba8e8bfd47
Yang Luo <hsluoyz@qq.com>
2017-04-11 21:22:24 +0800

    Use the testing framework.

---
+++ enforcer_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+"testing"
+"fmt"
+)
+
+func testEnforce(t *testing.T, e *Enforcer, sub string, obj string, act string, res bool) {
+	if e.enforce(sub, obj, act) != res {
+		t.Errorf("%s, %s, %s: %t, supposed to be %t", sub, obj, act, !res, res)
+	}
+}
+
+func TestBasicModel(t *testing.T) {
+	e := &Enforcer{}
+	e.init("examples/basic_model.conf", "examples/basic_policy.csv")
+
+	testEnforce(t, e, "alice", "data1", "read", true)
+	testEnforce(t, e, "alice", "data1", "write", false)
+	testEnforce(t, e, "alice", "data2", "read", false)
+	testEnforce(t, e, "alice", "data2", "write", false)
+	testEnforce(t, e, "bob", "data1", "read", false)
+	testEnforce(t, e, "bob", "data1", "write", false)
+	testEnforce(t, e, "bob", "data2", "read", false)
+	testEnforce(t, e, "bob", "data2", "write", true)
+}
+
+func TestBasicModelWithRoot(t *testing.T) {
+	e := &Enforcer{}
+	e.init("examples/basic_model_with_root.conf", "examples/basic_policy.csv")
+
+	testEnforce(t, e, "alice", "data1", "read", true)
+	testEnforce(t, e, "alice", "data1", "write", false)
+	testEnforce(t, e, "alice", "data2", "read", false)
+	testEnforce(t, e, "alice", "data2", "write", false)
+	testEnforce(t, e, "bob", "data1", "read", false)
+	testEnforce(t, e, "bob", "data1", "write", false)
+	testEnforce(t, e, "bob", "data2", "read", false)
+	testEnforce(t, e, "bob", "data2", "write", true)
+	testEnforce(t, e, "root", "data1", "read", true)
+	testEnforce(t, e, "root", "data1", "write", true)
+	testEnforce(t, e, "root", "data2", "read", true)
+	testEnforce(t, e, "root", "data2", "write", true)
+}
+
+func TestRBACModel(t *testing.T) {
+	e := &Enforcer{}
+	e.init("examples/rbac_model.conf", "examples/rbac_policy.csv")
+
+	testEnforce(t, e, "alice", "data1", "read", true)
+	testEnforce(t, e, "alice", "data1", "write", false)
+	testEnforce(t, e, "alice", "data2", "read", true)
+	testEnforce(t, e, "alice", "data2", "write", true)
+	testEnforce(t, e, "bob", "data1", "read", false)
+	testEnforce(t, e, "bob", "data1", "write", false)
+	testEnforce(t, e, "bob", "data2", "read", false)
+	testEnforce(t, e, "bob", "data2", "write", true)
+}
+
+func testKeyMatch(t *testing.T, e *Enforcer, key1 string, key2 string, res bool) {
+	my_res := e.keyMatch(key1, key2)
+	fmt.Print(key1 + ", " + key2 + ": ")
+	fmt.Println(my_res)
+
+	if my_res != res {
+		t.Errorf("%s < %s: %t, supposed to be %t", key1, key2, !res, res)
+	}
+}
+
+func TestKeyMatch(t *testing.T) {
+	e := &Enforcer{}
+
+	testKeyMatch(t, e, "/foo", "/foo", true)
+	testKeyMatch(t, e, "/foo", "/foo*", true)
+	testKeyMatch(t, e, "/foo", "/foo/*", false)
+	testKeyMatch(t, e, "/foo/bar", "/foo", false)
+	testKeyMatch(t, e, "/foo/bar", "/foo*", true)
+	testKeyMatch(t, e, "/foo/bar", "/foo/*", true)
+	testKeyMatch(t, e, "/foobar", "/foo", false)
+	testKeyMatch(t, e, "/foobar", "/foo*", true)
+	testKeyMatch(t, e, "/foobar", "/foo/*", false)
+}
---
+++ role_manager_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+	"fmt"
+)
+
+func testRole(t *testing.T, rm *RoleManager, name1 string, name2 string, res bool) {
+	my_res := rm.hasLink(name1, name2)
+	fmt.Print(name1 + ", " + name2 + ": ")
+	fmt.Println(my_res)
+
+	if my_res != res {
+		t.Errorf("%s < %s: %t, supposed to be %t", name1, name2, !res, res)
+	}
+}
+
+func TestRole(t *testing.T) {
+	rm := newRoleManager(3)
+	rm.addLink("u1", "g1")
+	rm.addLink("u2", "g1")
+	rm.addLink("u3", "g2")
+	rm.addLink("g1", "g3")
+
+	testRole(t, rm, "u1", "g1", true)
+	testRole(t, rm, "u1", "g2", false)
+	testRole(t, rm, "u1", "g3", true)
+	testRole(t, rm, "u2", "g1", true)
+	testRole(t, rm, "u2", "g2", false)
+	testRole(t, rm, "u2", "g3", true)
+	testRole(t, rm, "u3", "g1", false)
+	testRole(t, rm, "u3", "g2", true)
+	testRole(t, rm, "u3", "g3", false)
+}
