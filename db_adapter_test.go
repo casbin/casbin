ff9634b07d5c88ab5b07411152df0a21f1709a14
Yang Luo <hsluoyz@qq.com>
2017-04-19 08:52:58 +0800

    Improve the DB adapter test.

--- db_adapter_test.go
+++ db_adapter_test.go
@@ -2,23 +2,29 @@ package casbin
 
 import "testing"
 
-func TestDBLoadPolicy(t *testing.T) {
-	e := &Enforcer{}
-	e.Init("examples/basic_model.conf", "")
-
-	a := newDbAdapter("mysql", "root:@tcp(127.0.0.1:3306)/")
-	a.open()
-	a.loadPolicy(e.model)
-	printPolicy(e.model)
-	a.close()
-}
-
 func TestDBSavePolicy(t *testing.T) {
 	e := &Enforcer{}
-	e.Init("examples/basic_model.conf", "examples/basic_policy.csv")
+	e.Init("examples/rbac_model.conf", "examples/rbac_policy.csv")
 
 	a := newDbAdapter("mysql", "root:@tcp(127.0.0.1:3306)/")
 	a.open()
 	a.savePolicy(e.model)
+	a.close()
+}
+
+func TestDBSaveAndLoadPolicy(t *testing.T) {
+	e := &Enforcer{}
+	e.Init("examples/rbac_model.conf", "examples/rbac_policy.csv")
+
+	a := newDbAdapter("mysql", "root:@tcp(127.0.0.1:3306)/")
+	a.open()
+	a.savePolicy(e.model)
+
+	clearPolicy(e.model)
+	testGetPolicy(t, e, [][]string{})
+
+	a.loadPolicy(e.model)
+	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}})
+
 	a.close()
 }
