770aa1a8884b8434c43a298a808e09d4d592abb3
Yang Luo <hsluoyz@qq.com>
2017-04-22 19:16:52 +0800

    Add testing for deleting role links.

--- rbac/role_manager_test.go
+++ rbac/role_manager_test.go
@@ -33,6 +33,13 @@ func TestRole(t *testing.T) {
 	rm.AddLink("u4", "g3")
 	rm.AddLink("g1", "g3")
 
+	// Current role inheritance tree:
+	//             g3    g2
+	//            /  \  /  \
+	//          g1    u4    u3
+	//         /  \
+	//       u1    u2
+
 	testRole(t, rm, "u1", "g1", true)
 	testRole(t, rm, "u1", "g2", false)
 	testRole(t, rm, "u1", "g3", true)
@@ -51,6 +58,38 @@ func TestRole(t *testing.T) {
 	testPrintRoles(t, rm, "u3", []string{"g2"})
 	testPrintRoles(t, rm, "u4", []string{"g2", "g3"})
 	testPrintRoles(t, rm, "g1", []string{"g3"})
+	testPrintRoles(t, rm, "g2", []string{})
+	testPrintRoles(t, rm, "g3", []string{})
+
+
+	rm.DeleteLink("g1", "g3")
+	rm.DeleteLink("u4", "g2")
+
+	// Current role inheritance tree after deleting the links:
+	//             g3    g2
+	//               \     \
+	//          g1    u4    u3
+	//         /  \
+	//       u1    u2
+
+	testRole(t, rm, "u1", "g1", true)
+	testRole(t, rm, "u1", "g2", false)
+	testRole(t, rm, "u1", "g3", false)
+	testRole(t, rm, "u2", "g1", true)
+	testRole(t, rm, "u2", "g2", false)
+	testRole(t, rm, "u2", "g3", false)
+	testRole(t, rm, "u3", "g1", false)
+	testRole(t, rm, "u3", "g2", true)
+	testRole(t, rm, "u3", "g3", false)
+	testRole(t, rm, "u4", "g1", false)
+	testRole(t, rm, "u4", "g2", false)
+	testRole(t, rm, "u4", "g3", true)
+
+	testPrintRoles(t, rm, "u1", []string{"g1"})
+	testPrintRoles(t, rm, "u2", []string{"g1"})
+	testPrintRoles(t, rm, "u3", []string{"g2"})
+	testPrintRoles(t, rm, "u4", []string{"g3"})
+	testPrintRoles(t, rm, "g1", []string{})
 	testPrintRoles(t, rm, "g2", []string{})
 	testPrintRoles(t, rm, "g3", []string{})
 }
