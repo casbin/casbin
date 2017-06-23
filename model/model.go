b94799580bd61f4eeadf91722aec85e9ffc6b60e
Yang Luo <hsluoyz@qq.com>
2017-06-23 12:46:13 +0800

    Refactor out AddAssertion() API in model.

--- model/model.go
+++ model/model.go
@@ -37,9 +37,14 @@ var sectionNameMap = map[string]string{
 }
 
 func loadAssertion(model Model, cfg config.ConfigInterface, sec string, key string) bool {
+	value := cfg.String(sectionNameMap[sec] + "::" + key)
+	return AddAssertion(model, sec, key, value)
+}
+
+func AddAssertion(model Model, sec string, key string, value string) bool {
 	ast := Assertion{}
 	ast.Key = key
-	ast.Value = cfg.String(sectionNameMap[sec] + "::" + key)
+	ast.Value = value
 
 	if ast.Value == "" {
 		return false
