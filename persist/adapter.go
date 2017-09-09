c33657b4cb76947b1b829ad75cd4c09c021c97d7
Yang Luo <hsluoyz@qq.com>
2017-09-09 13:37:50 +0800

    Add comments to the adapter interface.

--- persist/adapter.go
+++ persist/adapter.go
@@ -39,10 +39,18 @@ func LoadPolicyLine(line string, model model.Model) {
 
 // Adapter is the interface for Casbin adapters.
 type Adapter interface {
+	// LoadPolicy loads all policy rules from the storage.
 	LoadPolicy(model model.Model) error
+	// SavePolicy saves all policy rules to the storage.
 	SavePolicy(model model.Model) error
 
+	// AddPolicy adds a policy rule to the storage.
+	// This is part of the Auto-Save feature.
 	AddPolicy(sec string, ptype string, rule []string) error
+	// RemovePolicy removes a policy rule from the storage.
+	// This is part of the Auto-Save feature.
 	RemovePolicy(sec string, ptype string, rule []string) error
+	// RemoveFilteredPolicy removes policy rules that match the filter from the storage.
+	// This is part of the Auto-Save feature.
 	RemoveFilteredPolicy(sec string, ptype string, fieldIndex int, fieldValues ...string) error
 }
