ffbe381d2eb4542c58d9bce94798476d742e0bfc
Yang Luo <hsluoyz@qq.com>
2018-08-06 21:39:45 +0800

    Add CachedEnforcer to cache the enforcement decisions.

---
+++ enforcer_cached_test.go
@@ -0,0 +1,52 @@
+// Copyright 2018 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package casbin
+
+import "testing"
+
+func testEnforceCache(t *testing.T, e *CachedEnforcer, sub string, obj interface{}, act string, res bool) {
+	t.Helper()
+	if e.Enforce(sub, obj, act) != res {
+		t.Errorf("%s, %v, %s: %t, supposed to be %t", sub, obj, act, !res, res)
+	}
+}
+
+func TestCache(t *testing.T) {
+	e := NewCachedEnforcer("examples/basic_model.conf", "examples/basic_policy.csv")
+	// The cache is enabled by default for NewCachedEnforcer.
+
+	testEnforceCache(t, e, "alice", "data1", "read", true)
+	testEnforceCache(t, e, "alice", "data1", "write", false)
+	testEnforceCache(t, e, "alice", "data2", "read", false)
+	testEnforceCache(t, e, "alice", "data2", "write", false)
+
+	// The cache is enabled, so even if we remove a policy rule, the decision
+	// for ("alice", "data1", "read") will still be true, as it uses the cached result.
+	e.RemovePolicy("alice", "data1", "read")
+
+	testEnforceCache(t, e, "alice", "data1", "read", true)
+	testEnforceCache(t, e, "alice", "data1", "write", false)
+	testEnforceCache(t, e, "alice", "data2", "read", false)
+	testEnforceCache(t, e, "alice", "data2", "write", false)
+
+	// Now we invalidate the cache, then all first-coming Enforce() has to be evaluated in real-time.
+	// The decision for ("alice", "data1", "read") will be false now.
+	e.InvalidateCache()
+
+	testEnforceCache(t, e, "alice", "data1", "read", false)
+	testEnforceCache(t, e, "alice", "data1", "write", false)
+	testEnforceCache(t, e, "alice", "data2", "read", false)
+	testEnforceCache(t, e, "alice", "data2", "write", false)
+}
