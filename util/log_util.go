eb26c29c5526f1369d23754ee14958c0735e1b26
Jordi Llonch <llonchj@gmail.com>
2018-10-31 04:09:52 +1100

    logger interface
    Signed-off-by: Cong Ding <masdc98@icloud.com>

--- util/log_util.go
+++ util/log_util.go
@@ -16,19 +16,58 @@ package util
 
 import "log"
 
-// EnableLog controls whether to print log to console.
-var EnableLog = true
+// Logger is the logging interface implementation.
+type Logger interface {
+	//EnableLog controls whether print the message.
+	EnableLog(bool)
+
+	//IsEnabled returns if logger is enabled.
+	IsEnabled() bool
+
+	//Print formats using the default formats for its operands and logs the message.
+	Print(...interface{})
+
+	//Printf formats according to a format specifier and logs the message.
+	Printf(string, ...interface{})
+}
+
+// DefaultLogger is the implementation for a Logger using golang log.
+type DefaultLogger struct {
+	enable bool
+}
+
+func (l *DefaultLogger) EnableLog(enable bool) { l.enable = enable }
+func (l *DefaultLogger) IsEnabled() bool       { return l.enable }
+
+func (l *DefaultLogger) Print(v ...interface{}) {
+	if l.enable {
+		log.Print(v...)
+	}
+}
+func (l *DefaultLogger) Printf(format string, v ...interface{}) {
+	if l.enable {
+		log.Printf(format, v...)
+	}
+}
+
+var logger Logger = &DefaultLogger{}
+
+// SetLogger sets the current logger.
+func SetLogger(l Logger) {
+	logger = l
+}
+
+// GetLogger returns the current logger.
+func GetLogger() Logger {
+	return logger
+}
 
 // LogPrint prints the log.
 func LogPrint(v ...interface{}) {
-	if EnableLog {
-		log.Print(v...)
-	}
+	logger.Print(v...)
 }
 
 // LogPrintf prints the log with the format.
 func LogPrintf(format string, v ...interface{}) {
-	if EnableLog {
-		log.Printf(format, v...)
-	}
+	logger.Printf(format, v...)
 }
