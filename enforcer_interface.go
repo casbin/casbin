39ad430a69558679fb0135b31c5473ab5d5a7f16
dovics <45029442+dovics@users.noreply.github.com>
2020-03-06 03:17:26 -0600

    Fix wrong return types for methods in IEnforcer (#373)
    * fix IEnforce wrong type for method
    
    * modify function type
    
    Signed-off-by: Cong Ding <masdc98@icloud.com>

--- enforcer_interface.go
+++ enforcer_interface.go
@@ -15,23 +15,27 @@
 package casbin
 
 import (
+	"github.com/Knetic/govaluate"
 	"github.com/casbin/casbin/v2/effect"
 	"github.com/casbin/casbin/v2/model"
 	"github.com/casbin/casbin/v2/persist"
 	"github.com/casbin/casbin/v2/rbac"
 )
 
+var _ IEnforcer = &Enforcer{}
+
+// IEnforcer is the API interface of Enforcer
 type IEnforcer interface {
 	/* Enforcer API */
-	InitWithFile(modelPath string, policyPath string)
-	InitWithAdapter(modelPath string, adapter persist.Adapter)
-	InitWithModelAndAdapter(m model.Model, adapter persist.Adapter)
-	LoadModel()
+	InitWithFile(modelPath string, policyPath string) error
+	InitWithAdapter(modelPath string, adapter persist.Adapter) error
+	InitWithModelAndAdapter(m model.Model, adapter persist.Adapter) error
+	LoadModel() error
 	GetModel() model.Model
 	SetModel(m model.Model)
 	GetAdapter() persist.Adapter
 	SetAdapter(adapter persist.Adapter)
-	SetWatcher(watcher persist.Watcher)
+	SetWatcher(watcher persist.Watcher) error
 	SetRoleManager(rm rbac.RoleManager)
 	SetEffector(eft effect.Effector)
 	ClearPolicy()
@@ -43,27 +47,27 @@ type IEnforcer interface {
 	EnableLog(enable bool)
 	EnableAutoSave(autoSave bool)
 	EnableAutoBuildRoleLinks(autoBuildRoleLinks bool)
-	BuildRoleLinks()
-	Enforce(rvals ...interface{}) bool
+	BuildRoleLinks() error
+	Enforce(rvals ...interface{}) (bool, error)
 
 	/* RBAC API */
 	GetRolesForUser(name string) ([]string, error)
 	GetUsersForRole(name string) ([]string, error)
 	HasRoleForUser(name string, role string) (bool, error)
-	AddRoleForUser(user string, role string) bool
-	AddPermissionForUser(user string, permission ...string) bool
-	DeletePermissionForUser(user string, permission ...string) bool
-	DeletePermissionsForUser(user string) bool
+	AddRoleForUser(user string, role string) (bool, error)
+	AddPermissionForUser(user string, permission ...string) (bool, error)
+	DeletePermissionForUser(user string, permission ...string) (bool, error)
+	DeletePermissionsForUser(user string) (bool, error)
 	GetPermissionsForUser(user string) [][]string
 	HasPermissionForUser(user string, permission ...string) bool
-	GetImplicitRolesForUser(name string, domain ...string) []string
-	GetImplicitPermissionsForUser(user string, domain ...string) [][]string
-	GetImplicitUsersForPermission(permission ...string) []string
-	DeleteRoleForUser(user string, role string) bool
-	DeleteRolesForUser(user string) bool
-	DeleteUser(user string) bool
-	DeleteRole(role string)
-	DeletePermission(permission ...string) bool
+	GetImplicitRolesForUser(name string, domain ...string) ([]string, error)
+	GetImplicitPermissionsForUser(user string, domain ...string) ([][]string, error)
+	GetImplicitUsersForPermission(permission ...string) ([]string, error)
+	DeleteRoleForUser(user string, role string) (bool, error)
+	DeleteRolesForUser(user string) (bool, error)
+	DeleteUser(user string) (bool, error)
+	DeleteRole(role string) (bool, error)
+	DeletePermission(permission ...string) (bool, error)
 
 	/* Management API */
 	GetAllSubjects() []string
@@ -84,19 +88,19 @@ type IEnforcer interface {
 	GetFilteredNamedGroupingPolicy(ptype string, fieldIndex int, fieldValues ...string) [][]string
 	HasPolicy(params ...interface{}) bool
 	HasNamedPolicy(ptype string, params ...interface{}) bool
-	AddPolicy(params ...interface{}) bool
-	AddNamedPolicy(ptype string, params ...interface{}) bool
-	RemovePolicy(params ...interface{}) bool
-	RemoveFilteredPolicy(fieldIndex int, fieldValues ...string) bool
-	RemoveNamedPolicy(ptype string, params ...interface{}) bool
-	RemoveFilteredNamedPolicy(ptype string, fieldIndex int, fieldValues ...string) bool
+	AddPolicy(params ...interface{}) (bool, error)
+	AddNamedPolicy(ptype string, params ...interface{}) (bool, error)
+	RemovePolicy(params ...interface{}) (bool, error)
+	RemoveFilteredPolicy(fieldIndex int, fieldValues ...string) (bool, error)
+	RemoveNamedPolicy(ptype string, params ...interface{}) (bool, error)
+	RemoveFilteredNamedPolicy(ptype string, fieldIndex int, fieldValues ...string) (bool, error)
 	HasGroupingPolicy(params ...interface{}) bool
 	HasNamedGroupingPolicy(ptype string, params ...interface{}) bool
-	AddGroupingPolicy(params ...interface{}) bool
-	AddNamedGroupingPolicy(ptype string, params ...interface{}) bool
-	RemoveGroupingPolicy(params ...interface{}) bool
-	RemoveFilteredGroupingPolicy(fieldIndex int, fieldValues ...string) bool
-	RemoveNamedGroupingPolicy(ptype string, params ...interface{}) bool
-	RemoveFilteredNamedGroupingPolicy(ptype string, fieldIndex int, fieldValues ...string) bool
-	AddFunction(name string, function func(args ...interface{}) (interface{}, error))
+	AddGroupingPolicy(params ...interface{}) (bool, error)
+	AddNamedGroupingPolicy(ptype string, params ...interface{}) (bool, error)
+	RemoveGroupingPolicy(params ...interface{}) (bool, error)
+	RemoveFilteredGroupingPolicy(fieldIndex int, fieldValues ...string) (bool, error)
+	RemoveNamedGroupingPolicy(ptype string, params ...interface{}) (bool, error)
+	RemoveFilteredNamedGroupingPolicy(ptype string, fieldIndex int, fieldValues ...string) (bool, error)
+	AddFunction(name string, function govaluate.ExpressionFunction)
 }
