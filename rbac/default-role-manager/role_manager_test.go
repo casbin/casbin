002d2d6b0663c0dc2280b895e4bb7a12f7a73d43
Divy Patel <divy9881@gmail.com>
2020-08-26 18:07:06 +0530

    fix: remove unwanted AddMatchingFunc and AddDomainMatchingFunc name argument. (#509)

--- rbac/default-role-manager/role_manager_test.go
+++ rbac/default-role-manager/role_manager_test.go
@@ -233,7 +233,7 @@ func TestClear(t *testing.T) {
 
 func TestDomainPartternRole(t *testing.T) {
 	rm := NewRoleManager(10)
-	rm.(*RoleManager).AddDomainMatchingFunc("keyMatch2", util.KeyMatch2)
+	rm.(*RoleManager).AddDomainMatchingFunc(util.KeyMatch2)
 
 	_ = rm.AddLink("u1", "g1", "domain1")
 	_ = rm.AddLink("u2", "g1", "domain2")
@@ -263,8 +263,8 @@ func TestDomainPartternRole(t *testing.T) {
 
 func TestAllMatchingFunc(t *testing.T) {
 	rm := NewRoleManager(10)
-	rm.(*RoleManager).AddMatchingFunc("keyMatch2", util.KeyMatch2)
-	rm.(*RoleManager).AddDomainMatchingFunc("keyMatch2", util.KeyMatch2)
+	rm.(*RoleManager).AddMatchingFunc(util.KeyMatch2)
+	rm.(*RoleManager).AddDomainMatchingFunc(util.KeyMatch2)
 
 	_ = rm.AddLink("/book/:id", "book_group", "*")
 	// Current role inheritance tree after deleting the links:
