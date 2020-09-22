a15979f6c65424154acc7a52a4a4afbab641d9fa
Zixuan Liu <nodeces@gmail.com>
2020-09-22 09:53:11 +0800

    revert: fix compatible RBAC in ABAC
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>
    Signed-off-by: Cong Ding <masdc98@icloud.com>

--- enforcer.go
+++ enforcer.go
@@ -446,11 +446,7 @@ func (e *Enforcer) enforce(matcher string, explains *[]string, rvals ...interfac
 				for _, ruleName := range ruleNames {
 					if j, ok := parameters.pTokens[ruleName]; ok {
 						rule := util.EscapeAssertion(pvals[j])
-						if strings.Contains(rule, ">") || strings.Contains(rule, "<") || strings.Contains(rule, "=") {
-							expWithRule = util.ReplaceEval(expWithRule, rule)
-						} else {
-							expWithRule = util.ReplaceEval(expWithRule, "false")
-						}
+						expWithRule = util.ReplaceEval(expWithRule, rule)
 					} else {
 						return false, errors.New("please make sure rule exists in policy when using eval() in matcher")
 					}
