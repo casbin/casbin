50e08de7df21edd341835acdb5a96e6e2fc1c6f7
closetool <4closetool3@gmail.com>
2021-05-04 12:07:06 +0800

    feat: add new rbac api
    Signed-off-by: closetool <4closetool3@gmail.com>

--- rbac_api_with_domains.go
+++ rbac_api_with_domains.go
@@ -58,3 +58,57 @@ func (e *Enforcer) DeleteRolesForUserInDomain(user string, domain string) (bool,
 
 	return e.RemoveGroupingPolicies(rules)
 }
+
+// GetAllUsersByDomain would get all users associated with the domain.
+func (e *Enforcer) GetAllUsersByDomain(domain string) []string {
+	m := make(map[string]struct{})
+	g := e.model["g"]["g"]
+	if len(g.Tokens) != 3 {
+		return []string{}
+	}
+	users := make([]string, 0)
+	for _, policy := range g.Policy {
+		if _, ok := m[policy[2]]; policy[2] == domain && ok {
+			users = append(users, policy[0])
+		}
+	}
+	return users
+}
+
+// DeleteAllUsersByDomain would delete all users associated with the domain.
+func (e *Enforcer) DeleteAllUsersByDomain(domain string) (bool, error) {
+	g := e.model["g"]["g"]
+	if len(g.Tokens) != 3 {
+		return false, nil
+	}
+	policies := make([][]string, 0)
+	for _, policy := range g.Policy {
+		if policy[3] == domain {
+			policies = append(policies, policy)
+		}
+	}
+	return e.RemoveGroupingPolicies(policies)
+}
+
+// DeleteDomains would delete all associated users and roles.
+// It would delete all domains if parameter is not provided.
+func (e *Enforcer) DeleteDomains(domains ...string) (bool, error) {
+	g := e.model["g"]["g"]
+	if len(g.Tokens) != 3 {
+		return false, nil
+	}
+	if len(domains) == 0 {
+		return e.RemoveGroupingPolicies(g.Policy)
+	}
+	m := make(map[string]struct{})
+	for _, domain := range domains {
+		m[domain] = struct{}{}
+	}
+	policies := make([][]string, 0)
+	for _, policy := range g.Policy {
+		if _, ok := m[policy[2]]; ok {
+			policies = append(policies, policy)
+		}
+	}
+	return e.RemoveGroupingPolicies(policies)
+}
