a925c5f75ed7d72fed739f22ec5d6d5be2a60609
OneOfOne <oneofone@gmail.com>
2023-04-11 12:54:00 -0500

    fix: crash when Clear is called between locks

--- persist/cache/cache_sync.go
+++ persist/cache/cache_sync.go
@@ -29,9 +29,9 @@ func (c *SyncCache) Set(key string, value bool, extra ...interface{}) error {
 	if len(extra) > 0 {
 		ttl = extra[0].(time.Duration)
 	}
-	(*c).Lock()
-	defer (*c).Unlock()
-	(*c).cache[key] = cacheItem{
+	c.Lock()
+	defer c.Unlock()
+	c.cache[key] = cacheItem{
 		value:     value,
 		expiresAt: time.Now().Add(ttl),
 		ttl:       ttl,
@@ -40,16 +40,16 @@ func (c *SyncCache) Set(key string, value bool, extra ...interface{}) error {
 }
 
 func (c *SyncCache) Get(key string) (bool, error) {
-	(*c).RLock()
-	res, ok := (*c).cache[key]
-	(*c).RUnlock()
+	c.RLock()
+	res, ok := c.cache[key]
+	c.RUnlock()
 	if !ok {
 		return false, ErrNoSuchKey
 	} else {
 		if res.ttl > 0 && time.Now().After(res.expiresAt) {
-			(*c).Lock()
-			defer (*c).Unlock()
-			delete((*c).cache, key)
+			c.Lock()
+			defer c.Unlock()
+			delete(c.cache, key)
 			return false, ErrNoSuchKey
 		}
 		return res.value, nil
@@ -57,24 +57,23 @@ func (c *SyncCache) Get(key string) (bool, error) {
 }
 
 func (c *SyncCache) Delete(key string) error {
-	(*c).RLock()
-	_, ok := (*c).cache[key]
-	(*c).RUnlock()
+	c.RLock()
+	_, ok := c.cache[key]
+	c.RUnlock()
 	if !ok {
 		return ErrNoSuchKey
 	} else {
-		(*c).Lock()
-		defer (*c).Unlock()
-		delete((*c).cache, key)
+		c.Lock()
+		defer c.Unlock()
+		delete(c.cache, key)
 		return nil
 	}
 }
 
 func (c *SyncCache) Clear() error {
-	*c = SyncCache{
-		make(DefaultCache),
-		sync.RWMutex{},
-	}
+	c.Lock()
+	c.cache = make(DefaultCache)
+	c.Unlock()
 	return nil
 }
 
