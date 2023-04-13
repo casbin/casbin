e0dc85887208dfc712375cbd32dc0b1ef10d8b8b
鱼竿钓鱼干 <851892190@qq.com>
2023-04-13 17:13:20 +0800

    fix: fix words

--- errors/rbac_errors.go
+++ errors/rbac_errors.go
@@ -25,6 +25,6 @@ var (
 	INVALID_FIELDVAULES_PARAMETER = errors.New("fieldValues requires at least one parameter")
 
 	// AccessibleBy errors
-	ERR_OBJ_ACCESSIBLEBY          = errors.New("AccessibleBy need obj conditions lick 'r.obj.conditions'")
+	ERR_OBJ_ACCESSIBLEBY          = errors.New("AccessibleBy need obj conditions like 'r.obj.conditions'")
 	ERR_EMPTY_RESULT_ACCESSIBLEBY = errors.New("AccessibleBy have an empty result")
 )
