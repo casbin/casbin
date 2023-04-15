f1056999824aa7ea609dea853dc3e848c5c7ca0c
hsluoyz <hsluoyz@qq.com>
2023-04-15 21:12:42 +0800

    Update rbac_api_test.go

--- rbac_api_test.go
+++ rbac_api_test.go
@@ -15,12 +15,13 @@
 package casbin
 
 import (
-	"github.com/casbin/casbin/v2/constant"
-	"github.com/casbin/casbin/v2/errors"
-	"github.com/casbin/casbin/v2/util"
 	"log"
 	"sort"
 	"testing"
+
+	"github.com/casbin/casbin/v2/constant"
+	"github.com/casbin/casbin/v2/errors"
+	"github.com/casbin/casbin/v2/util"
 )
 
 func testGetRoles(t *testing.T, e *Enforcer, res []string, name string, domain ...string) {
