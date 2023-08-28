46e7aa069fda7b59d8f4ba86284ae71c2de0525f
hsluoyz <hsluoyz@qq.com>
2023-08-28 13:09:54 +0800

    Update adapter_context.go

--- persist/adapter_context.go
+++ persist/adapter_context.go
@@ -24,18 +24,18 @@ import (
 type ContextAdapter interface {
 	Adapter
 
-	// LoadPolicy loads all policy rules from the storage with context.
+	// LoadPolicyCtx loads all policy rules from the storage with context.
 	LoadPolicyCtx(ctx context.Context, model model.Model) error
-	// SavePolicy saves all policy rules to the storage with context.
+	// SavePolicyCtx saves all policy rules to the storage with context.
 	SavePolicyCtx(ctx context.Context, model model.Model) error
 
-	// AddPolicy adds a policy rule to the storage with context.
+	// AddPolicyCtx adds a policy rule to the storage with context.
 	// This is part of the Auto-Save feature.
 	AddPolicyCtx(ctx context.Context, sec string, ptype string, rule []string) error
-	// RemovePolicy removes a policy rule from the storage with context.
+	// RemovePolicyCtx removes a policy rule from the storage with context.
 	// This is part of the Auto-Save feature.
 	RemovePolicyCtx(ctx context.Context, sec string, ptype string, rule []string) error
-	// RemoveFilteredPolicy removes policy rules that match the filter from the storage with context.
+	// RemoveFilteredPolicyCtx removes policy rules that match the filter from the storage with context.
 	// This is part of the Auto-Save feature.
 	RemoveFilteredPolicyCtx(ctx context.Context, sec string, ptype string, fieldIndex int, fieldValues ...string) error
 }
