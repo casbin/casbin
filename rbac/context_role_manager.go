80ee5f43ce89ca6953cad25cc913c17be0d89f8d
hsluoyz <hsluoyz@qq.com>
2023-08-28 14:27:16 +0800

    Update context_role_manager.go

--- rbac/context_role_manager.go
+++ rbac/context_role_manager.go
@@ -1,14 +1,27 @@
+// Copyright 2023 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package rbac
 
-import (
-	"context"
-)
+import "context"
 
 // ContextRoleManager provides a context-aware interface to define the operations for managing roles.
 // Prefer this over RoleManager interface for context propagation, which is useful for things like handling
 // request timeouts.
 type ContextRoleManager interface {
 	RoleManager
+
 	// ClearCtx clears all stored data and resets the role manager to the initial state with context.
 	ClearCtx(ctx context.Context) error
 	// AddLinkCtx adds the inheritance link between two roles. role: name1 and role: name2 with context.
