cbf928d5bd9ca06b3183a509f0be49c8f52f3122
Yang Luo <hsluoyz@gmail.com>
2026-08-21 14:41:37 +0800

    Update util.go

--- util/util.go
+++ util/util.go
@@ -392,10 +392,9 @@ func (cache *LRUCache) Get(key interface{}) (value interface{}, ok bool) {
 
 func (cache *LRUCache) Put(key interface{}, value interface{}) {
 	if cache.capacity <= 0 {
-		// A cache with non-positive capacity holds nothing, so there is nothing
-		// to store and no eviction to perform.
 		return
 	}
+
 	n, ok := cache.m[key]
 	if ok {
 		cache.remove(n, false)
